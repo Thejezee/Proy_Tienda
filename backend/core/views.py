10d2fab43f2ed62037056f9878b587ac89cf96d9
Thejezee <joaquinarteaga945@gmail.com>
2025-11-24 21:14:38 -0400

    Refactor lista_productos to use Stock model

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from .models import Producto, Stock
+from .models import Stock
 
 def lista_productos(request):
-    # 1. Sacamos todos los productos de la BD
-    productos = Producto.objects.all()
-    
-    # 2. Se los mandamos al HTML (que crearemos luego)
-    return render(request, 'core/lista_productos.html', {
-        'mis_productos': productos
-    })
+    stocks = Stock.objects.select_related('producto', 'producto__categoria', 'producto__proveedor').all()
+    return render(request, 'core/lista_productos.html', {'stocks': stocks})
