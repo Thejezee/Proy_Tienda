29b2da8573b7fc48bfe9e14bff96554834c0d2d8
Thejezee <joaquinarteaga945@gmail.com>
2025-11-25 15:11:56 -0400

    Add route for lista_productos view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from core.views import lista_productos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lista_productos, name='inicio'),  # ← Esta línea es la clave
 ]
