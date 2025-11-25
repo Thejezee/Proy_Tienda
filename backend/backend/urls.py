0996880181b52ec201019994586bf64f4848bfe2
Thejezee <joaquinarteaga945@gmail.com>
2025-11-25 13:25:43 -0400

    Remove lista_productos route from URLs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import lista_productos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lista_productos, name='inicio'),
 ]
